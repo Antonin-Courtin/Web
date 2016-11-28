6a54977dc08f271c6a67ba58f28fd4a92d6343cb
Antonin-Courtin <antonin.cour@live.fr>
2016-11-28 05:27:49 +0100

    ajout de la modification des données personnelles

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -20,7 +20,43 @@ class UserModel {
 		else
 			return false;
 	}
+    public function getCommandesUser($idUser){
+        $queryBuilder = new QueryBuilder($this->db);
+        $queryBuilder
+            ->select('*')
+            ->from('commandes')
+            ->where('user_id = ?')
+            ->innerJoin('commandes','etats', 't','etat_id=t.id')
+            ->setParameter(0, $idUser)
+            ->addOrderBy('t.libelle', 'DESC');;
 
+        return $queryBuilder->execute()->fetchAll();
+
+}
+
+    public function updateUser($donnees){
+        $queryBuilder = new QueryBuilder($this->db);
+        $queryBuilder
+            ->update('users','p')
+            ->set('p.login','?')
+            ->set('p.ville','?')
+            ->set('p.password','?')
+            ->set('p.code_postal','?')
+            ->set('p.nom','?')
+            ->set('p.adresse','?')
+            ->set('p.Email','?')
+            ->where('p.id=?')
+            ->setParameter(0,$donnees['login'])
+            ->setParameter(1,$donnees['ville'])
+            ->setParameter(2,$donnees['password'])
+            ->setParameter(3,$donnees['code_postal'])
+            ->setParameter(4,$donnees['nom'])
+            ->setParameter(5,$donnees['adresse'])
+            ->setParameter(6,$donnees['Email'])
+            ->setParameter(7,$donnees['id'])
+            ;
+        $queryBuilder->execute();
+    }
 	public function getUser($user_id) {
 		$queryBuilder = new QueryBuilder($this->db);
 		$queryBuilder
